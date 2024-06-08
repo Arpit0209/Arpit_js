3b5260ca516c6907c6bb9271d0cdc37c1e22d0d8
Arpitjavascript <atcy105@gmail.com>
2024-06-08 10:51:48 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+console.log(3 +3)
+
+console.log("arpit")
+
+let accountlogin = true
+
+console.log(accountlogin);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+//console.log(typeof undefined); // undefined
+//console.log(typeof null); // object
