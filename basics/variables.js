4518790a82e26b4c1781a98b1cc8791b58a3f528
Arpitjavascript <atcy105@gmail.com>
2024-06-08 09:45:39 +0000

    conf

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 125666
+let email = "arpitc11@gmail.com"
+var pass = "22222"
+city = "jaipur"
+let state
+//console.log(accountId);
+
+email = "c11@gmail.com"
+pass = "3333"
+city = "bhopal"
+
+/*
+never user var in java script due to  blockage issue in scopes
+
+if value of variable is not defined then the output should be Undefined
+*/
+
+console.table([accountId, email, pass, city, state])
