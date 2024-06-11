09cf5845d04dcd0eb78ca4eefa5d920936e35947
Arpitjavascript <atcy105@gmail.com>
2024-06-11 12:07:45 +0000

    changes

--- basics/A.js
+++ basics/A.js
@@ -24,8 +24,8 @@ console.log(b);
 
 //////////////////////////////////
 
-const a = [1, 2, 3]
-const b = a
-b.push(4)
-console.log(a)
+// const a = [1, 2, 3]
+// const b = a
+// b.push(4)
+// console.log(a)
 
