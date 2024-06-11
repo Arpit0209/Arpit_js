6523433a5d947542f005622eca146a651c7b5559
Arpitjavascript <atcy105@gmail.com>
2024-06-11 12:04:31 +0000

    changes

--- basics/A.js
+++ basics/A.js
@@ -1,7 +1,23 @@
-let x = "a"
-let y = x
- y = "b"
+// let x = "a"
+// let y = x
+//  y = "b"
 
 
-console.log(x);
-console.log(y);
+// console.log(x);
+// console.log(y);
+
+const a = [
+
+    {name: "arpit"},
+   { name: "chouksey"
+}
+
+]
+const b = [...a]
+
+b[0] = {name: "vaibhav"};
+b[1].age = 30;
+
+console.log(a);
+console.log(b);
+
